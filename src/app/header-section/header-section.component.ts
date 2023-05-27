546164423740b705cb5f509b2c35297c54284160
myinfo <chetraj0101@gmail.com>
2023-05-27 01:02:57 -0500

    pushing file

---
+++ src/app/header-section/header-section.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-section',
+  templateUrl: './header-section.component.html',
+  styleUrls: ['./header-section.component.css']
+})
+export class HeaderSectionComponent {
+
+  /*
+   clickMe(): void {
+      alert("Hello Angular to Javascript")
+  }
+  */
+
+}
